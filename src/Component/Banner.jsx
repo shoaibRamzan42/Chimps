9fbe0d434e122451242f55b730523979ecc86cc7
shoaibRamzan42 <adnanrmzan420@gmail.com>
2024-01-30 16:43:48 +0500

    updated

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
 import { IconButton } from "@mui/material";
+import { Button } from "@material-tailwind/react";
+ 
 
 
 
@@ -12,20 +14,20 @@ const Banner = () => {
   return (
     <>
      
-      <div className="flex flex-col justify-center mt-40 items-center gap-5">
-      <button className="px-8 py-4 bg-white rounded-lg shadow-lg shadow-cyan-600 text-[12px] md:text-[16px] font-extrabold ">
-            Minted NFT : 480/11569
-          </button>
+      <div className="flex flex-col justify-center absolute left-[35%] top-[60%] items-center gap-5">
+      <h1 className="px-8 py-4  bg-white cursor-text rounded-lg shadow-lg shadow-cyan-600 text-[12px] md:text-[16px] font-extrabold ">
+      Minted NFT : 480/11569
+          </h1>
         <div className="flex gap-6 justify-center items-center">
         
-          <button onClick={ count> 0 ?()=> setCount(count - 1) : count}  className="bg-white px-2 py-2 rounded-2xl">
+          <button onClick={ count> 0 ?()=> setCount(count - 1) : count}  className="bg-white  hover:scale-105 duration-300 px-2 py-2 rounded-2xl">
             <IconButton>
               <RemoveIcon style={{ color: "black", fontSize: 30 }} />
             </IconButton>
           </button>
           {/* <span class="text-2xl font-bold">{{ count }}</span> */}
           <h1 className="text-2xl text-white font-bold"> {count} </h1>
-          <button onClick={ count>= 0 ?()=> setCount(count + 1) : count}  className="bg-white px-2 py-2 rounded-2xl">
+          <button onClick={ count>= 0 ?()=> setCount(count + 1) : count}  className="bg-white hover:scale-105 duration-300 px-2 py-2 rounded-2xl">
             <IconButton>
             
               <AddIcon style={{ color: "black", fontSize: 30 }} />
@@ -34,12 +36,13 @@ const Banner = () => {
         </div>
 
         <div className="flex gap-3">
-          <button className="md:px-20 sm:px-12  py-4 bg-white rounded-full text-[12px] md:text-[16px] font-extrabold ">
+          <button className="md:px-20 hover:shadow-md hover:shadow-cyan-700 duration-300 sm:px-12  py-4 bg-white rounded-full text-[12px] md:text-[16px] font-extrabold ">
             Mint
           </button>
-          <button className="md:px-20 sm:px-12  py-4 bg-sky-300 rounded-full text-[12px] md:text-[16px] font-extrabold ">
+          <button className="md:px-20 hover:shadow-md hover:shadow-gray-300 duration-300 sm:px-12  py-4 bg-cyan-300 rounded-full text-[12px] md:text-[16px] font-extrabold ">
             Mint
           </button>
+          
         </div>
         </div>
   
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -3,11 +3,13 @@ import React from "react";
 
 const Navbar=()=>{
     return(<>
+        <nav className="bg-black/20 ">
         <div className="flex justify-end  px-[2%] py-[2%] ">
-        <button className="px-8 py-4 bg-white rounded-full text-[12px] md:text-[16px] font-extrabold ">
+        <button className="px-8 py-4 bg-white shadow-lg shadow-cyan-700 hover:scale-110 duration-300 rounded-full text-[12px] md:text-[16px] font-extrabold ">
            Connect Wallet
           </button>
         </div>
+        </nav>
     </>)
 }
 export default Navbar;
